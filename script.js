c8ffa2a7473eff5f9834c0557251f4fb68e1d417
ItsAnkitPatel <itsakppatel@gmail.com>
2024-04-29 12:24:51 +0530

    cleaner code & add comments for readability

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
 const allImages = Array.from(document.querySelectorAll("img"));
-allImages.forEach((img) => {
-  img.addEventListener("click", checkOnClick);
-});
+
 let topImage = allImages[allImages.length - 1];
 topImage.className = "top-layer";
 
-let toggle = false;
-let dblToggle = false;
-let rotateDblToggle = false;
+let singleClickToggle = false;
+let doubleClickToggle = false;
+let rotateDoubleClickToggle = false;
 let canChooseTopImage = false;
 
-const fourImgPositions = [
-  "top-left",
-  "top-right",
-  "bottom-left",
-  "bottom-right",
-];
-const fourImgPositionsAndRotate = [
+const IMG_POSITIONS = ["top-left", "top-right", "bottom-left", "bottom-right"];
+const IMG_POSITIONS_AND_ROTATE = [
   "top-left",
   "top-left-rotate",
   "top-right",
@@ -26,48 +19,65 @@ const fourImgPositionsAndRotate = [
   "bottom-right",
   "bottom-right-rotate",
 ];
-function singleClick(e) {
+
+function filterArrayFnc() {
+  return allImages.filter((img) => img !== topImage);
+}
+
+function handleSingleClick(e) {
   const filteredArray = filterArrayFnc();
   e.stopImmediatePropagation();
-  if (toggle === false && dblToggle === false) {
+  // when single click active & double click event is inactive
+  // add 10 degree in decrement order to make them look like cards
+  if (singleClickToggle === false && doubleClickToggle === false) {
     filteredArray.forEach((img, i) => {
-      img.classList.add("hover-transition");
-      img.classList.add(`rotate-${i + 1}`);
+      img.classList.add("hover-transition", `rotate-${i + 1}`);
     });
-    toggle = true;
-  } else if (!rotateDblToggle && dblToggle) {
+    singleClickToggle = true;
+    // When double clicked event happened and user clicked once on the center image
+    // rotate the image with other images
+  } else if (!rotateDoubleClickToggle && doubleClickToggle) {
     filteredArray.forEach((img, i) => {
-      img.classList.add(fourImgPositionsAndRotate[2 * i + 1]);
+      img.classList.add(IMG_POSITIONS_AND_ROTATE[2 * i + 1]);
     });
-    topImage.classList.add("top-layer-dblclick")
-    rotateDblToggle = true;
-  } else if (rotateDblToggle && dblToggle) {
+    topImage.classList.add("top-layer-dblclick");
+    rotateDoubleClickToggle = true;
+
+    // When double click event active and images are rotated
+    // then remove the classes and make the all images rotate 0 deg
+  } else if (rotateDoubleClickToggle && doubleClickToggle) {
     filteredArray.forEach((img, i) => {
-      img.classList.remove(fourImgPositionsAndRotate[2 * i + 1]);
+      img.classList.remove(IMG_POSITIONS_AND_ROTATE[2 * i + 1]);
     });
-    topImage.classList.remove("top-layer-dblclick")
-    rotateDblToggle = false;
-  } else {
-    toggle = false;
+    topImage.classList.remove("top-layer-dblclick");
+    rotateDoubleClickToggle = false;
+  } 
+  // reset the rotate
+  else {
+    singleClickToggle = false;
     filteredArray.forEach((img, i) =>
-      img.classList.remove(`rotate-${i + 1}`, "hover-transition")
+      img.classList.remove("hover-transition", `rotate-${i + 1}`)
     );
   }
 }
-topImage.addEventListener("click", singleClick);
-function checkOnClick(e) {
 
+// This will handle image swap to choose the top image
+function handleImageClick(e) {
   if (canChooseTopImage && e.target.classList.contains("top-layer") === false) {
     e.stopImmediatePropagation();
+
     topImage.classList.remove("top-layer");
     topImage.className = e.target.getAttribute("class");
-    topImage.removeEventListener("dblclick", applyDblClick);
-    topImage.removeEventListener("click", singleClick);
+
+    topImage.removeEventListener("dblclick", handleDoubleClick);
+    topImage.removeEventListener("click", handleSingleClick);
+
     topImage = e.target;
-    topImage.addEventListener("dblclick", applyDblClick);
-    topImage.addEventListener("click", singleClick);
+    topImage.addEventListener("dblclick", handleDoubleClick);
+    topImage.addEventListener("click", handleSingleClick);
     topImage.className = "top-layer";
-    dblToggle = true;
+    doubleClickToggle = true;
+
     const filteredArray = filterArrayFnc();
     filteredArray.forEach((img) => {
       img.classList.remove(
@@ -81,43 +91,51 @@ function checkOnClick(e) {
         "bottom-right-rotate"
       );
     });
+
     canChooseTopImage = false;
+    singleClickToggle = false;
     forFalseDblClick(filteredArray);
-    // applyDblClick(e);
-    toggle = false;
   }
 }
-function filterArrayFnc() {
-  return allImages.filter((img) => img !== topImage);
-}
-
+// When double click event is active run this
 function forTrueDblClick(filteredArray) {
   canChooseTopImage = true;
   filteredArray.forEach((img, i) => {
     img.classList.remove("hover-transition", `rotate-${i + 1}`, `img-${i + 1}`);
-    img.classList.add(fourImgPositions[i], "four-transition");
+    img.classList.add(IMG_POSITIONS[i], "four-transition");
   });
-  dblToggle = true;
+  doubleClickToggle = true;
 }
+
+// When double click event going to inactive run this
 function forFalseDblClick(filteredArray) {
   canChooseTopImage = false;
   filteredArray.forEach((img, i) => {
     img.classList.remove(
-      fourImgPositionsAndRotate[2 * i],
-      fourImgPositionsAndRotate[2 * i + 1],
+      IMG_POSITIONS_AND_ROTATE[2 * i],
+      IMG_POSITIONS_AND_ROTATE[2 * i + 1],
       "four-transition"
     );
     img.classList.add(`img-${i + 1}`);
   });
-  dblToggle = false;
+  topImage.classList.remove("top-layer-dblclick");
+  doubleClickToggle = false;
 }
-function applyDblClick(e) {
+
+// Handle main double click event order
+function handleDoubleClick(e) {
   const filteredArray = filterArrayFnc();
   e.stopImmediatePropagation();
-  if (dblToggle === false) {
+  if (doubleClickToggle === false) {
     forTrueDblClick(filteredArray);
   } else {
     forFalseDblClick(filteredArray);
   }
 }
-topImage.addEventListener("dblclick", applyDblClick);
+
+// All event listeners
+allImages.forEach((img) => {
+  img.addEventListener("click", handleImageClick);
+});
+topImage.addEventListener("dblclick", handleDoubleClick);
+topImage.addEventListener("click", handleSingleClick);
